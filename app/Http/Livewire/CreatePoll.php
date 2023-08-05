818990dc222d6efb86154ad6b6f85d822b27e27a
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2023-08-04 22:05:32 -0300

    Componente de criar Poll

--- app/Http/Livewire/CreatePoll.php
+++ app/Http/Livewire/CreatePoll.php
@@ -6,8 +6,23 @@ use Livewire\Component;
 
 class CreatePoll extends Component
 {
+    public $title;
+    public $options = [''];
+
     public function render()
     {
         return view('livewire.create-poll');
     }
+
+    public function addOption() {
+        $this->options[] = '';
+    }
+
+    public function removeOption($index) {
+        unset($this->options[$index]);
+    }
+
+//    public function mount()
+//    {
+//    }
 }
--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,3 +1,35 @@
 <div>
-    {{-- Nothing in the world is as soft and yielding as water. --}}
+    <form>
+        <label>Nome da enquete</label>
+        <input
+            type="text"
+            wire:model.lazy="title"
+        />
+
+        <div class="mb-4 mt-5">
+            <button
+                class="btn"
+                wire:click.prevent="addOption"
+            >
+                Adicionar opção
+            </button>
+        </div>
+
+        <div>
+            @foreach($options as $index => $option)
+                <div class="mb-4 flex flex-row gap-y-4 items-center justify-between">
+                    <label>{{ $index + 1 }}</label>
+                    <input
+                        type="text"
+                        wire:model.lazy="options.{{ $index }}"
+                    />
+                    <button
+                        class="btn"
+                        wire:click.prevent="removeOption({{ $index }})"
+
+                    >Remove</button>
+                </div>
+            @endforeach
+        </div>
+    </form>
 </div>
